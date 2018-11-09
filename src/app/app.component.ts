c94744697240958dc8eba5ac4a0f0b82926fb7bf
Brian Kopp <briankopp.usa@gmail.com>
2018-11-09 05:45:24 -0700

    updating app component and module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TreeService } from './services/tree.service';
+import { TreeItem } from './model/treeitem';
+import { HierarchyItem } from './model/hierachyitem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  treeItems: HierarchyItem[];
+  constructor(private treeItemService: TreeService) {}
+
+  ngOnInit() {
+    this.treeItems = this.treeItemService.getItems();
+  }
 }
